fdada9d94d14df0ef0b90a0e6f8c3184a79ebfe0
malshan <malshan@ideahub.lk>
2019-09-12 09:23:57 +0800

    Initial routing pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    XyzComponent
   ],
   imports: [
     BrowserModule,
